abb45c1f7ff7b9dc3f6253732f58e63c8bcec41a
tmoore1210 <tmmoore1210@gmail.com>
2022-10-03 23:39:31 -0500

    removeFavourites and Local Storage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
 import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -25,6 +26,28 @@ const App = () => {
  useEffect(() => {
   getMovieRequest(searchValue);
  }, [searchValue]);
+
+ useEffect(()=>{
+  const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favourites')
+  );
+  setFavourites(movieFavourites)
+ }, []);
+
+ const saveToLocalStorage = (items) => {
+  localStorage.setItem('react-movie-app-favourites', JSON.stringify(items))
+ };
+
+ const addFavouriteMovie = (movie) => {
+    const newFavouriteList = [...favourites, movie]
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+ };
+
+ const removeFavouriteMovie = (movie) => {
+    const newFavouriteList = favourites.filter((favourite=> favourite.imdbID !== movie.imdbID));
+    setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
+ };
   
   return (
   <div className='container-fluid movie-app'>
@@ -33,7 +56,13 @@ const App = () => {
       <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
     </div>
     <div className='row'>
-        <MovieList movies = {movies} favouriteComponent={AddFavourites}/>
+        <MovieList movies = {movies} handleFavouritesClick={addFavouriteMovie} favouriteComponent={AddFavourites}/>
+    </div>
+    <div className='row d-flex align-items-center mt-4 mb-4'>
+      <MovieListHeading heading='Favourites'/>
+    </div>
+    <div className='row'>
+        <MovieList movies = {favourites} handleFavouritesClick={removeFavouriteMovie} favouriteComponent={RemoveFavourites}/>
     </div>
   </div>
   );
